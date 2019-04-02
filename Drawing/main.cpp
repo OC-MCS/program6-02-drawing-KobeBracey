e9ba35407a610e3be09efd306c17e06cf6e4c195
Kobe Bracey <kobe.bracey@eagles.oc.edu>
2019-04-02 12:29:45 -0500

    finished

--- Drawing/main.cpp
+++ Drawing/main.cpp
@@ -16,17 +16,17 @@ using namespace sf;
 
 int main()
 {
-	const int WINDOW_WIDTH = 800; // Window width.
-	const int WINDOW_HEIGHT = 500; // Window height.
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGHT = 500; 
 
-	fstream binaryFile; // Binary file to maintain data.
-	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Drawing"); // Window.
+	fstream binaryFile;
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Drawing"); 
 	window.setFramerateLimit(60);
 
-	SettingsMgr settingsMgr(Color::Transparent, ShapeEnum::NOTHING); // Maintains the current drawing setting.
-	SettingsUI  settingsUI(&settingsMgr); // Left side of screen: Handle mouse clicks on setting buttons.
-	ShapeMgr    shapeMgr; // Maintains the list of shapes that have been created. Reads/writes the data to file.
-	DrawingUI   drawingUI(Vector2f(200, 50)); // Right side of screen: Draws on screen.
+	SettingsMgr settingsMgr(Color::Transparent, ShapeEnum::NOTHING);
+	SettingsUI settingsUI(&settingsMgr); 
+	ShapeMgr shapeMgr; 
+	DrawingUI drawingUI(Vector2f(200, 50)); 
 
 	binaryFile.open("shapes.bin", ios::in | ios::binary);
 	if (binaryFile)
